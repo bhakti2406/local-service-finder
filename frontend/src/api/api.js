478d3eb305e34232e3fc939cf150853d694bcf50
Bhakti Sudhir <bhakti.ps24@gmail.com>
2025-12-28 15:13:42 +0400

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+const api = axios.create({
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.authorization = `Bearer ${token}`;
-  }
-  return req;
-});
+api.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user?.token) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
