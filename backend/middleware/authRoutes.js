a01a3c564a5f4effd85d0ba7c90867bf6dc3f6fb
Bhakti Sudhir <bhakti.ps24@gmail.com>
2025-12-28 14:12:04 +0400

    Create authRoutes.js

---
+++ backend/middleware/authRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getMyProfile,
+  updateMyProfile,
+} from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+const router = express.Router();
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getMyProfile);
+router.put("/profile", protect, updateMyProfile);
+export default router;
