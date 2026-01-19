c85d98403fb6ea272f2111548d15612d240f9c4e
Bhakti Sudhir <bhakti.ps24@gmail.com>
2026-01-19 22:16:42 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,20 +1,22 @@
+// src/api/axios.js
 import axios from "axios";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 api.interceptors.request.use(
   (config) => {
     const user = JSON.parse(localStorage.getItem("user"));
-
     if (user?.token) {
       config.headers.Authorization = `Bearer ${user.token}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error)
 );
+
 export default api;
